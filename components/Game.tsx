a6ed8e0e874e777e90ccbd6b7a3d18899748a0e8
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-22 20:57:52 +0600

    Changed bg of game

--- components/Game.tsx
+++ components/Game.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Game = ({ game_id }: Props) => {
   return (
-    <div className='px-5 py-2 flex items-center justify-between odd:bg-zinc-200 rounded-md'>
+    <div className='px-5 py-2 flex items-center justify-between odd:bg-zinc-800 rounded-md'>
       <span>{game_id}</span>
       <Link href={`/${game_id}`}>
         <a className='px-3 py-1 bg-black text-white'>Join</a>
