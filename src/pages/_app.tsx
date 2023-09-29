b3b094af02429227ca7149ddbee412e8a587da50
Md Shahriyar Alam <contact@shahriyar.dev>
2023-09-30 04:17:34 +0600

    added header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import WsProvider from "@/socket/SocketContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WsProvider>
       <Toaster />
+      <Head>
+        <title>Tic Tac Toe</title>
+        <meta name="description" content="A online multiplayer tic-tac-toe game" />
+        <meta name="Author" content="shahriyardx" />
+      </Head>
       <Component {...pageProps} />
     </WsProvider>
   );
