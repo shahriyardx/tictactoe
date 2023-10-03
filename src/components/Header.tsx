191a501d1b912b55a1e7a2f8bdd50f1dca6e9ca5
Md Shahriyar Alam <contact@shahriyar.dev>
2023-10-03 22:03:41 +0600

    changed header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image"
 import React from "react"
 
 const Header = ({ online }: { online: number }) => {
   return (
     <div>
+      <div className="flex justify-center items-center">
+        <Image src="/ttt.png" width={300} height={300} alt="banner" />
+      </div>
       <div className="flex justify-between items-center mb-5 mt-10">
         <h1 className="text-4xl font-bold text-center">
           Tic Tac Toe
