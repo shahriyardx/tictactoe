4fae07c5a53b1a885b6a5b072303233b6ee3469f
Md Shahriyar Alam <contact@shahriyar.dev>
2023-10-03 22:01:33 +0600

    removed image from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image"
 import React from "react"
 
 const Header = ({ online }: { online: number }) => {
   return (
     <div>
-      <div className="flex justify-center items-center">
-        <Image src="/banner.png" width={200} height={200} alt="banner" />
-      </div>
       <div className="flex justify-between items-center mb-5 mt-10">
         <h1 className="text-4xl font-bold text-center">
           Tic Tac Toe
