a0a5c6af3ceb34d8a1fed0802cbe77cf115d60d9
Md Shahriyar Alam <contact@shahriyar.dev>
2023-10-03 20:20:11 +0600

    added profile component

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -8,50 +8,47 @@ type Props = {
   oponent: Player & { avatar: string }
 }
 
+type ProfileProps = Omit<Props, "oponent"> & {
+  reverse?: boolean
+}
+
+const Profile = ({ user, reverse, currentTurn }: ProfileProps) => {
+  return (
+    <div
+      className={`grid ${
+        reverse
+          ? "grid-cols-[auto,35px] sm:grid-cols-[auto,80px]"
+          : "grid-cols-[35px,auto] sm:grid-cols-[80px,auto]"
+      } gap-2 sm:gap-5`}
+    >
+      <Image
+        src={(user.avatar as string) || "/pfp.png"}
+        width={80}
+        height={80}
+        alt="Avatar"
+        className={`rounded-md ${reverse ? "order-2" : ""}`}
+      />
+      <div
+        className={`flex flex-col justify-center ${reverse ? "items-end" : ""}`}
+      >
+        <span className="sm:text-2xl">{user.name}</span>
+        <span className="text-xs sm:text-base">
+          {currentTurn === user.id ? (
+            <span className="text-green-500">Turn now</span>
+          ) : (
+            <span className="text-yellow-500">wait</span>
+          )}
+        </span>
+      </div>
+    </div>
+  )
+}
+
 const GameHeader = ({ currentTurn, user, oponent }: Props) => {
   return (
     <div className="flex items-center justify-between bg-black p-3 rounded-md">
-      <div className="grid grid-cols-[35px,auto] sm:grid-cols-[80px,auto] gap-2 sm:gap-5">
-        <Image
-          src={(user.avatar as string) || "/pfp.png"}
-          width={80}
-          height={80}
-          alt="Avatar"
-          className="rounded-md"
-        />
-        <div className="flex flex-col justify-center">
-          <span className="sm:text-2xl">{user.name}</span>
-          <span className="text-xs sm:text-base">
-            {currentTurn === user.id ? (
-              <span className="text-green-500">Turn now</span>
-            ) : (
-              <span className="text-yellow-500">wait</span>
-            )}
-          </span>
-        </div>
-      </div>
-
-      {oponent.id && (
-        <div className="grid grid-cols-[auto,35px] sm:grid-cols-[auto,80px] gap-2 sdm:gap-5">
-          <div className="flex flex-col justify-center items-end">
-            <span className="sm:text-2xl">{oponent.name}</span>
-            <span className="text-xs sm:text-base">
-              {currentTurn === oponent.id ? (
-                <span className="text-green-500">Turn now</span>
-              ) : (
-                <span className="text-yellow-500">wait</span>
-              )}
-            </span>
-          </div>
-          <Image
-            src={(oponent.avatar as string) || "/pfp.png"}
-            width={80}
-            height={85}
-            alt="Avatar"
-            className="rounded-md"
-          />
-        </div>
-      )}
+      <Profile user={user} currentTurn={currentTurn} />
+      <Profile user={oponent} currentTurn={currentTurn} reverse />
     </div>
   )
 }
